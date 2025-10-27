735ecaa8d9fc87519c3f3f281c97ab93f2f91528
kalilinuxuser <161124933+0x06K@users.noreply.github.com>
2025-10-27 21:55:33 +0500

    os-development

--- kernel/arch_x86_64/gdt_idt/gdt.c
+++ kernel/arch_x86_64/gdt_idt/gdt.c
@@ -1,8 +1,8 @@
-// Global Discriptor Table - this time implementing with my own two hands.
+// GDT - this time implementing with my own two hands.
 
 #include "gdt.h"
 #include<stdint.h>
-
+// GDT ENTRY
 typedef struct {
     uint16_t LimitLow;        // Segment limit (bits 0–15)
     uint16_t BaseLow;         // Base address (bits 0–15)
@@ -11,3 +11,10 @@ typedef struct {
     uint8_t  LimitHigh_Flags; // High 4 bits of limit + flags (granularity, size)
     uint8_t  BaseHigh;        // Base address (bits 24–31)
 } __attribute__((packed)) GLOBAL_DISCRIPTOR_ENTRY;
+
+// GDT Pointer
+typedef struct {
+    uint8_t Limit;
+    uint16_t Base;
+} __attribute__(packed) GDT_PTR;
+
